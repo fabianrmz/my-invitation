4e1b643346505b0fccc93e81abb5868726e087a1
Fabián Ramírez <actuallyfabian@gmail.com>
2025-01-10 19:35:11 -0600

    fix: add initial mockup card

---
+++ src/components/card/Card.js
@@ -0,0 +1,20 @@
+import './Card.css';
+
+function Card() {
+    return (
+      <div className="card-border">
+        <p className="initials">A &F</p>
+        <div className="card-content">
+          <p>CELEBRA CON NOSOTROS ESTE DIA ESPECIAL</p>
+          <p className="bold">AMARIANY & FABIÁN</p>
+          <p>Jueves 13 de Febrero 13:00hrs</p>
+          <a href="geo:20.732775,-103.442099" target="_blank">
+            <p>Av. Sta Margarita 4950... </p>
+          </a>
+        </div>
+        
+      </div>
+    );
+  }
+  
+  export default Card;
