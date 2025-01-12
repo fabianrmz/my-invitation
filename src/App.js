98ec0c4393b9ebbe6d2ffd6813ddf3317023e00d
Fabián Ramírez <actuallyfabian@gmail.com>
2025-01-11 18:46:32 -0600

    fix: correct sizes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import iconBack from './media/back-icon.svg';
+
 
 import Card from './components/card/Card';
 
@@ -7,9 +9,13 @@ import Card from './components/card/Card';
 function App() {
   return (
     <main>
-        <div className="flip-card">
-          <Card side="card-front"></Card>
-          <Card side="card-back"></Card>
+      
+      <div className="flip-card">
+        <div className="back-icon">
+          <img src={ iconBack } alt="Back Icon"/>
+        </div>
+        <Card side="card-front"></Card>
+        <Card side="card-back"></Card>
       </div>
     </main>
   );
